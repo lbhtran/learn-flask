c0d7bd83ea1e9e8ccf23f2db21c62f50972e9571
Alexx <lbhtran@gmail.com>
2019-01-04 21:06:55 +0000

    Chapter 11 facelift

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -12,6 +13,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
