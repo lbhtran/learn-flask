e5968b6dc115c5ddeae47b1b846a6e1879833b11
Alexx <lbhtran@gmail.com>
2019-01-14 20:53:13 +0000

    Extra: rewriting Lucky

--- app/games/lucky.py
+++ app/games/lucky.py
@@ -4,3 +4,22 @@ import random
 
 def roll_a_dice():
     return random.randint(1,6)
+
+class Dice(object):
+    def __init__(self):
+        self.roll()
+
+    def roll(self):
+        return random.randint(1, 6)
+
+class Lucky(object):
+    def __init__(self):
+        self.humanplayer = Dice().roll()
+        self.computerplayer = Dice().roll()
+
+        if self.humanplayer > self.computerplayer:
+            self.win = True
+        elif self.humanplayer < self.computerplayer:
+            self.lose = True
+        else:
+            self.draw = True
