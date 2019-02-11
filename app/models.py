1420db067d9108a7f9375f15e20eb87894f86557
Alexx <lbhtran@gmail.com>
2019-02-11 21:33:01 +0000

    Chapter 23: API - user authentication (getting error, has sent comment to Miguel for help)

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from hashlib import md5
 from time import time
 from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.search import add_to_index, remove_from_index, query_index
-import jwt, json
+import jwt, json, base64
 import redis
-import rq
+import rq, os
 from app import db, login
 
 class SearchableMixin(object):
@@ -89,6 +89,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     scores = db.relationship('Score', backref='player', uselist=False)
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -104,6 +106,7 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     notifications = db.relationship('Notification', backref='user',
                                     lazy='dynamic')
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -207,6 +210,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
